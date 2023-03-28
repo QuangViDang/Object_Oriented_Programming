0acfaf85e67e7d74d0692cfceda413f3f62a4754
Dang Quang Vi <vidangquang1203@gmail.com>
2023-03-28 17:10:26 +0700

    Create Bai5.java

---
+++ BTTH_Tuan1/Bai5.java
@@ -0,0 +1,36 @@
+package BT_TH.BTTH_Tuan1;
+
+import java.util.Arrays;
+
+public class Bai5 {
+    public static void main(String[] args) {
+        // Nhập vào dãy số thực từ tham số dòng lệnh
+        double[] arr = new double[args.length];
+        for (int i = 0; i < args.length; i++) {
+            arr[i] = Double.parseDouble(args[i]);
+        }
+        
+        // Hiển thị dãy đã nhập
+        System.out.print("Dãy số đã nhập: ");
+        for (double num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+        
+        // Sắp xếp dãy và hiển thị dãy đã sắp xếp
+        Arrays.sort(arr);
+        System.out.print("Dãy số đã sắp xếp: ");
+        for (double num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+        
+        // Tính trung bình cộng của dãy và hiển thị kết quả
+        double sum = 0;
+        for (double num : arr) {
+            sum += num;
+        }
+        double avg = sum / arr.length;
+        System.out.println("Trung bình cộng của dãy: " + avg);
+    }
+}
